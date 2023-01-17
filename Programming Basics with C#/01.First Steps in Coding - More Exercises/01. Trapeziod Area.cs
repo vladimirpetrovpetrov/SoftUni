0c55451d49d330676d49b16764e6557aed2e5219
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-01-17 03:37:18 +0200

    Updating

---
+++ Programming Basics with C#/01.First Steps in Coding - More Exercises/01. Trapeziod Area.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Тренировка
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var a = double.Parse(Console.ReadLine());
+			var b = double.Parse(Console.ReadLine());
+			var h = double.Parse(Console.ReadLine());
+
+			var result = (a + b) * (h / 2);
+			Console.WriteLine(String.Format("{0:0.00}", result));
+		}
+	}
+}
---
+++ Programming Basics with C#/01.First Steps in Coding - More Exercises/10. Weather Forecast - Part 2.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Тренировка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = double.Parse(Console.ReadLine());
+            if (temp >= 5 && temp <= 11.9)
+            {
+                Console.WriteLine("Cold");
+            }
+            else if (temp >= 12 && temp <= 14.9)
+            {
+                Console.WriteLine("Cool");
+            }
+            else if (temp >= 15 && temp <= 20)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if (temp >= 20.1 && temp <= 25.9)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (temp >= 26 && temp <= 35)
+            {
+                Console.WriteLine("Hot");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+
+
+
+
+
+        }
+    }
+}
