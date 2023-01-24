d193e34b0d6231ee5789983c9b7763583e97b3fb
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-01-24 23:07:52 +0200

    little fix problem 9

--- C# Fundamentals/Basic Syntax/Basic Syntax - Lab/9. Sum of Odd Numbers/Program.cs
+++ C# Fundamentals/Basic Syntax/Basic Syntax - Lab/9. Sum of Odd Numbers/Program.cs
@@ -1,7 +1,11 @@
-﻿for (int i = 0; i < 100; i++)
+﻿var n = int.Parse(Console.ReadLine());
+int number = 1;
+int result = 0;
+while (n > 0)
 {
-    //65
-    ;
-
-    Console.WriteLine(i);
+    result += number;
+    Console.WriteLine(number);
+    number += 2;
+    n--;
 }
+Console.WriteLine($"Sum: {result}");
