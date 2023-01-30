59465ef2808611888ba0d442fd794ddbf90579fc
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-01-30 23:29:59 +0200

    TEST

---
+++ C# Fundamentals/Arrays/Arrays- Exercise/test/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] x = new int[] {3,1,2};
+var reversedX = new int[3];
+reversedX = x.Reverse().ToArray(); 
+Console.WriteLine(String.Join(" ",reversedX)); //2,1,3
+
+
+
+
+Array.Reverse(x); // тук го завърта и го сейфа
+Console.WriteLine(String.Join(" ", x));
