1e7df16b7ab22f31d316c113f16ca3303bb3f4d6
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-01-31 05:49:07 +0200

    problem 6 - solved

--- C# Fundamentals/Arrays/Arrays- Exercise/test/Program.cs
+++ C# Fundamentals/Arrays/Arrays- Exercise/test/Program.cs
@@ -1,10 +1,8 @@
-﻿int[] x = new int[] {3,1,2};
-var reversedX = new int[3];
-reversedX = x.Reverse().ToArray(); 
-Console.WriteLine(String.Join(" ",reversedX)); //2,1,3
+﻿using System.Collections.Immutable;
+using System.Linq;
 
+int[] x = new int[] {3,1,2};
 
-
-
-Array.Reverse(x); // тук го завърта и го сейфа
-Console.WriteLine(String.Join(" ", x));
+var y = x.OrderBy(x => x);
+Console.WriteLine(String.Join(" ", y));
+Console.WriteLine(y);
