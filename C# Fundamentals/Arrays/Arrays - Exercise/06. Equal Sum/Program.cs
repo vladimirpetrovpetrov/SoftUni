4079ee2684c92ce3c7109441d00bb09a4139598f
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-01-31 05:59:33 +0200

    Rearrange

--- C# Fundamentals/Arrays/Arrays - Exercise/06. Equal Sum/Program.cs
+++ C# Fundamentals/Arrays/Arrays - Exercise/06. Equal Sum/Program.cs
@@ -1,1 +1,25 @@
-﻿//TODO
+﻿int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+bool areEqual = false;
+
+for(int i = 0;i < ints.Length; i++)
+{
+    int sumL = 0;
+    int sumR = 0;
+    for (int j = 0; j < i; j++)
+    {
+        sumL += ints[j];
+    }
+    for (int k = ints.Length-1; k > i; k--)
+    {
+        sumR += ints[k];
+    }
+    if(sumL == sumR)
+    {
+        areEqual = true;
+        Console.WriteLine(i);
+    }
+}
+if (!areEqual)
+{
+    Console.WriteLine("no");
+}
