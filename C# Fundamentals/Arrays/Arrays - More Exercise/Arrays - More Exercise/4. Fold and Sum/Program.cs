fa2483aee8a7796de94807c83c0a3ea7f885b11d
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-02-03 07:50:43 +0200

    problem 4 & 5

---
+++ C# Fundamentals/Arrays/Arrays - More Exercise/Arrays - More Exercise/4. Fold and Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int k = 0;
+
+int[] leftArr = new int[ints.Length / 4];
+int[] rightArr = new int[ints.Length / 4];
+
+for (int i = 0;i < leftArr.Length; i++)
+{
+    leftArr[i] = ints[i];
+}
+for (int i = ints.Length-1; i > (ints.Length-1) - rightArr.Length ; i--)
+{
+    rightArr[k] = ints[i];
+    k++;
+}
+k = 0;
+Array.Reverse(leftArr);
+int[] firstRow = new int[ints.Length / 2];
+for (int i = 0; i < firstRow.Length/2; i++)
+{
+    firstRow[i] = leftArr[i];
+}
+for (int i = firstRow.Length/2; i <= firstRow.Length-1; i++)
+{
+    firstRow[i] = rightArr[k];
+    k++;
+}
+int[] secondRow = new int[ints.Length / 2];
+Array.Copy(ints,leftArr.Length,secondRow,0,secondRow.Length);
+int[] result = new int[secondRow.Length];
+
+for (int i = 0; i < secondRow.Length; i++)
+{
+    result[i] = firstRow[i] + secondRow[i];
+}
+
+Console.WriteLine(String.Join(" ",result));
---
+++ C# Fundamentals/Arrays/Arrays - More Exercise/Arrays - More Exercise/5. Longest Increasing Subsequence (LIS)/Program.cs
@@ -0,0 +1,53 @@
+﻿var s = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int maxSeq = 1;
+int seqNow = 1;
+
+int biggerIndex = 0;
+bool done = false;
+
+for (int i = 0;i < s.Length; i++)
+{
+    int k = i+1;
+    if(k == s.Length - 1)
+    {
+        break;
+    }
+    while(done == false)
+    {
+        if (k == s.Length - 1)
+        {
+            done = true;
+        }
+        seqNow = MaxSeq(s,s[i], k, s.Length);
+        if (seqNow > maxSeq)
+        {
+            maxSeq = seqNow;
+        }
+        k++;
+    }
+    done = false;
+}
+Console.WriteLine(maxSeq);
+
+int MaxSeq(int[] s,int proverqvanoChislo ,int i,int length)
+{
+    int seqM = 1;
+    int seqN = 1;
+    for (int j = i; j < length-1; j++)
+    {
+        if (proverqvanoChislo < s[j])
+        {
+            seqN++;
+
+            if (seqN > seqM)
+            {
+                seqM = seqN;
+            }
+        } 
+    }
+
+
+    return seqM;
+}
+;
