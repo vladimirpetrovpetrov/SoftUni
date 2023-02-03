e1a6ec45bc1d197e9f8c530f51635670ce55d385
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-02-03 06:24:27 +0200

    problem 3 recursion fibonacci

---
+++ C# Fundamentals/Arrays/Arrays - More Exercise/Arrays - More Exercise/3. Recursive Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetFibonacci(n));
+
+
+
+int GetFibonacci(int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    }
+
+   return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+}
