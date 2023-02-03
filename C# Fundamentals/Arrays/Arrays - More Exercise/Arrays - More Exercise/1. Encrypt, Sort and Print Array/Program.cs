dc4b8f8fd75d8f6033f99e1e840a602f0960ced9
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-02-03 06:16:09 +0200

    problem 1 problem 2

---
+++ C# Fundamentals/Arrays/Arrays - More Exercise/Arrays - More Exercise/1. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,29 @@
+﻿var n = int.Parse(Console.ReadLine());
+List<int> list = new List<int>();
+for(int i = 0;i < n; i++)
+{
+
+
+    var command = Console.ReadLine();
+    int sum = 0;
+    foreach (char item in command)
+	{
+		if(item =='a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' || 
+
+            item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U' )
+		{
+			sum += (int)item * command.Length;
+		}
+		else
+		{
+			sum += (int)item / command.Length;
+		}
+    }
+	list.Add(sum);
+	sum = 0;
+}
+int[] y = new int[list.Count];
+foreach(int i in list.OrderBy(x => x))
+{
+	Console.WriteLine(i);
+}
---
+++ C# Fundamentals/Arrays/Arrays - More Exercise/Arrays - More Exercise/2. Pascal Triangle/Program.cs
@@ -0,0 +1,44 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+int k = 1;
+int v = 2;
+int[] ints = CreateArray(v);
+InitArray(ints);
+if(n == 0)
+{
+    return;
+}
+else if(n == 1)
+{
+    Console.WriteLine(1);
+    return;
+}
+
+Console.WriteLine(1);
+Console.WriteLine(String.Join(" ",ints));
+for (int i = 2; i < n; i++)
+{
+    v++;
+    int[] intsNew = new int[v];
+    InitArray(intsNew);
+
+    for (int j = 1; j < ints.Length; j++)
+    {
+        intsNew[j] = ints[j-1] + ints[j];
+    }
+    
+    ints = intsNew;
+    Console.WriteLine(String.Join(" ", intsNew));
+
+    
+}
+
+
+int[] CreateArray (int length)
+{
+    return new int[length];
+}
+void InitArray(int[] array)
+{
+    Array.Fill(array, 1);
+}
