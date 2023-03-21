5dbbd167655a1412c8f35a29ecc90a979c225482
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-03-21 21:44:17 +0200

    Fixed Problem 3
    Rewrote the code  for Problem 3 from previous Exam.

--- C# Fundamentals/Regualr Final Exam Training/01. Programming Fundamentals Final Exam/03. The Pianist/Program.cs
+++ C# Fundamentals/Regualr Final Exam Training/01. Programming Fundamentals Final Exam/03. The Pianist/Program.cs
@@ -1,28 +1,16 @@
-﻿var n = int.Parse(Console.ReadLine());
-var pianoPieces = new Dictionary<string, Dictionary<string, string>>();
+﻿int n = int.Parse(Console.ReadLine());
+var pieces = new Dictionary<string, List<string>>();
 for (int i = 0; i < n; i++)
 {
     var input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
     var piece = input[0];
     var composer = input[1];
     var key = input[2];
-
-    if (!pianoPieces.ContainsKey(composer)) //IF composer doesnt exist
+    pieces[piece] = new List<string>()
     {
-        var song = new Dictionary<string, string>();
-        song[piece] = key;
-        pianoPieces[composer] = song;
-    }
-    else //IF composer exist
-    {
-        if(!pianoPieces[composer].ContainsKey(piece)) ////IF composer exist,but piece doesnt exist
-        {
-            var song = new Dictionary<string, string>();
-            song[piece] = key;
-            pianoPieces[composer].Add(piece, key);
-        }
-    }
-} 
+        composer,key
+    };
+}
 while (true)
 {
     var input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
@@ -30,81 +18,44 @@ while (true)
     {
         break;
     }
-
-    if (input[0] == "Remove")
+    if (input[0] == "Add")
     {
-        var pieceToRemove = input[1];
-        bool exist = false;
-        foreach (var (key,value) in pianoPieces)
+        var piece = input[1];
+        var composer = input[2];
+        var key = input[3];
+        if (pieces.ContainsKey(piece))
         {
-            if (value.ContainsKey(pieceToRemove))//If the collection have this piece
-            {
-                exist = true;
-                value.Remove(pieceToRemove);
-                Console.WriteLine($"Successfully removed {pieceToRemove}!");
-                break; 
-            }
+            Console.WriteLine($"{piece} is already in the collection!");
+            continue;
         }
-        if (!exist)//If the collection dont have this piece
-        {
-            Console.WriteLine($"Invalid operation! {pieceToRemove} does not exist in the collection.");
-        }
+        pieces.Add(piece, new List<string>() { composer, key });
+        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
     }
-    else if (input[0] == "Add")
+    else if (input[0] == "Remove")
     {
-        var pieceToAdd = input[1];
-        var composerToAdd = input[2];
-        var keyToAdd = input[3];
-        if(!pianoPieces.ContainsKey(composerToAdd)) //If the composer doesnt exist
+        var piece = input[1];
+        if (!pieces.ContainsKey(piece))
         {
-            var song = new Dictionary<string, string>();
-            song[pieceToAdd] = keyToAdd;
-            pianoPieces[composerToAdd] = song;
-            Console.WriteLine($"{pieceToAdd} by {composerToAdd} in {keyToAdd} added to the collection!");
+            Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+            continue;
         }
-        else //If the composer exist
-        {
-            //Composer exists , but not the piece
-            if (!pianoPieces[composerToAdd].ContainsKey(pieceToAdd))
-            {
-                var song = new Dictionary<string, string>();
-                song[pieceToAdd] = keyToAdd;
-                pianoPieces[composerToAdd].Add(pieceToAdd, keyToAdd);
-                Console.WriteLine($"{pieceToAdd} by {composerToAdd} in {keyToAdd} added to the collection!");
-            }
-            else //If composer and piece , both exist.
-            {
-                Console.WriteLine($"{pieceToAdd} is already in the collection!");
-            }
-        }
+        pieces.Remove(piece);
+        Console.WriteLine($"Successfully removed {piece}!");
     }
     else if (input[0] == "ChangeKey")
     {
-        var pieceToChange = input[1];
-        var newKey = input[2];
-        var exist = false;
-        foreach (var (key, value) in pianoPieces)
+        var piece = input[1];
+        var key = input[2];
+        if (!pieces.ContainsKey(piece))
         {
-            if(value.ContainsKey(pieceToChange)) //If the collection have this piece
-            {
-                exist = true;
-                value[pieceToChange] = newKey;
-                Console.WriteLine($"Changed the key of {pieceToChange} to {newKey}!");
-                break;
-            }
+            Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+            continue;
         }
-        if (!exist) //If the collection dont have this piece
-        {
-            Console.WriteLine($"Invalid operation! {pieceToChange} does not exist in the collection.");
-        }
+        pieces[piece][1] = key;
+        Console.WriteLine($"Changed the key of {piece} to {key}!");
     }
 }
-pianoPieces = pianoPieces.Where(x => x.Value.Count != 0).ToDictionary(x => x.Key,x => x.Value); //Remove the composers with 0 songs from the dictionary
-
-foreach (var (key,value) in pianoPieces)
+foreach (var (key, value) in pieces)
 {
-    foreach (var (k,v) in value)
-    {
-        Console.WriteLine($"{k} -> Composer: {key}, Key: {value[k]}");
-    }
+    Console.WriteLine($"{key} -> Composer: {value[0]}, Key: {value[1]}");
 }
