550dd953796c8d7877fa985a3043e8f62438a213
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-04-24 21:34:48 +0300

    Multidimensional Array - Exercise
    Updated Problem 7 and Problem 10

--- C# Advanced/02. Multidimensional Arrays/Multidimensional Arrays - Exercise/07. Knight Game/Program.cs
+++ C# Advanced/02. Multidimensional Arrays/Multidimensional Arrays - Exercise/07. Knight Game/Program.cs
@@ -8,7 +8,116 @@ for (int row = 0; row < n; row++)
 		chess[row,col] = input[col];
 	}
 }
-;
-//Try also with jagged array
+var minimumRemoves = 0;
 
+while (true)
+{
+    var biggestCount = 0;
+    var knightRow = 0;
+    var knightCol = 0;
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            var count = 0;
+            count += CheckLong(chess, row, col);
+            count += CheckShort(chess, row, col);
 
+            if (count > biggestCount)
+            {
+                biggestCount = count;
+                knightRow = row;
+                knightCol = col;
+            }
+        }
+    }
+    if(biggestCount == 0)
+    {
+        Console.WriteLine(minimumRemoves);
+        break;
+    }
+    chess[knightRow, knightCol] = '0';
+    minimumRemoves++;
+}
+static int CheckShort(char[,] chess, int row, int col)
+{
+    var count = 0;
+    if (chess[row, col] == 'K')
+    {
+        //SHORT UP LEFT 
+        if (row > 0 && col > 1)
+        {
+            if (chess[row - 1, col - 2] == 'K')
+            {
+                count++;
+            }
+        }
+        //SHORT UP RIGHT
+        if (row > 0 && col < chess.GetLength(1) - 2)
+        {
+            if (chess[row - 1, col + 2] == 'K')
+            {
+                count++;
+            }
+        }
+        //SHORT DOWN LEFT
+        if (row < chess.GetLength(1) - 1 && col > 1)
+        {
+            if (chess[row + 1, col - 2] == 'K')
+            {
+                count++;
+            }
+        }
+        //SHORT DOWN RIGHT
+        if (row < chess.GetLength(1) - 1 && col < chess.GetLength(1) - 2)
+        {
+            if (chess[row + 1, col + 2] == 'K')
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+static int CheckLong(char[,] chess, int row, int col)
+{
+    var count = 0;
+    if (chess[row, col] == 'K')
+    {
+        //LONG UP LEFT 
+        if (row > 1 && col > 0)
+        {
+            if (chess[row - 2, col - 1] == 'K')
+            {
+                count++;
+            }
+        }
+        //LONG UP RIGHT
+        if (row > 1 && col < chess.GetLength(1) - 1)
+        {
+            if (chess[row - 2, col + 1] == 'K')
+            {
+                count++;
+            }
+        }
+        //LONG DOWN LEFT
+        if (row < chess.GetLength(1) - 2 && col > 0)
+        {
+            if (chess[row + 2, col - 1] == 'K')
+            {
+                count++;
+            }
+        }
+        //LONG DOWN RIGHT
+        if (row < chess.GetLength(1) - 2 && col < chess.GetLength(1) - 1)
+        {
+            if (chess[row + 2, col + 1] == 'K')
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
--- C# Advanced/02. Multidimensional Arrays/Multidimensional Arrays - Exercise/10. Radioactive Mutant Vampire Bunnies/Program.cs
+++ C# Advanced/02. Multidimensional Arrays/Multidimensional Arrays - Exercise/10. Radioactive Mutant Vampire Bunnies/Program.cs
@@ -1,13 +1,12 @@
 ﻿var sizes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 var rows = sizes[0];
 var cols = sizes[1];
-
 var matrix = new char[rows, cols];
-
-var startingRow = 0;
-var startingColumn = 0;
-
-//Initializing the matrix
+var playerRow = 0;
+var playerCol = 0;
+List<string> currentBunnies = new List<string>();
+List<string> allBunnies = new List<string>();
+//Initializing the matrix.
 for (int row = 0; row < rows; row++)
 {
     var input = Console.ReadLine().ToCharArray();
@@ -15,267 +14,65 @@ for (int row = 0; row < rows; row++)
     {
         if (input[col] == 'P')
         {
-            startingRow = row;
-            startingColumn = col;
+            //Initializing starting player coords
+            playerRow = row;
+            playerCol = col;
+        }
+        else if (input[col] == 'B')
+        {
+            currentBunnies.Add($"{row},{col}");
         }
         matrix[row, col] = input[col];
-    }
-}
 
-//Finding indexes of the initial bunnies
-List<string> currentBunnies = new List<string>();
-List<string> allBunnies = new List<string>(currentBunnies);
-for (int row = 0; row < matrix.GetLength(0); row++)
-{
-    for (int col = 0; col < matrix.GetLength(1); col++)
-    {
-        if (matrix[row, col] == 'B')
-        {
-            currentBunnies.Add($"{row}-{col}");
-        }
     }
 }
 allBunnies.AddRange(currentBunnies);
-var directions = Console.ReadLine().ToCharArray();
-bool hasWon = false;
-bool hasDied = false;
-var counter = 0;
-//Main loop(Player and Bunny moving)
-while(!hasWon && !hasDied)
-{
-    //Player moving
-    if (directions[counter] == 'L')
-    {
-        if (startingColumn == 0)
-        {
-            hasWon = true;
-            matrix[startingRow, startingColumn] = '.';
-            break;
-        }else if (matrix[startingRow,startingColumn-1] == 'B')
-        {
-            hasDied = true;
-            matrix[startingRow, startingColumn] = '.';
-            startingColumn--;
-            break;
-        }
-        matrix[startingRow, startingColumn] = '.';
-        startingColumn--;
-        matrix[startingRow, startingColumn] = 'P';
-        //един път ако ние настъпим заек и един пък ще направя после ако заек ме засъпи мен
-    }
-    else if (directions[counter] == 'R')
-    {
-        if (startingColumn == matrix.GetLength(1)-1)
-        {
-            hasWon = true;
-            matrix[startingRow, startingColumn] = '.';
-            break;
-        }
-        else if (matrix[startingRow, startingColumn + 1] == 'B')
-        {
-            hasDied = true;
-            matrix[startingRow, startingColumn] = '.';
-            startingColumn++;
-            break;
-        }
-        matrix[startingRow, startingColumn] = '.';
-        startingColumn++;
-        matrix[startingRow, startingColumn] = 'P';
-    }
-    else if (directions[counter] == 'U')
-    {
-        if (startingRow == 0)
-        {
-            hasWon = true;
-            matrix[startingRow, startingColumn] = '.';
-            break;
-        }
-        else if (matrix[startingRow-1, startingColumn] == 'B')
-        {
-            hasDied = true;
-            matrix[startingRow, startingColumn] = '.';
-            startingRow--;
-            break;
-        }
-        matrix[startingRow, startingColumn] = '.';
-        startingRow--;
-        matrix[startingRow, startingColumn] = 'P';
-    }
-    else if (directions[counter] == 'D')
-    {
-        if (startingRow == matrix.GetLength(0) - 1)
-        {
-            hasWon = true;
-            matrix[startingRow, startingColumn] = '.';
-            break;
-        }
-        else if (matrix[startingRow + 1, startingColumn] == 'B')
-        {
-            hasDied = true;
-            matrix[startingRow, startingColumn] = '.';
-            startingRow++;
-            break;
-        }
-        matrix[startingRow, startingColumn] = '.';
-        startingRow++;
-        matrix[startingRow, startingColumn] = 'P';
-    }
-    counter++;
-    //--------------------------------------------------------
-    //Bunny moving
-    for (int j = 0; j < currentBunnies.Count; j++)
-    {
-        var curBunnyRow = int.Parse(currentBunnies[j].Split("-", StringSplitOptions.RemoveEmptyEntries)[0]);
-        var curBunnyCol = int.Parse(currentBunnies[j].Split("-", StringSplitOptions.RemoveEmptyEntries)[1]);
-        //LEFT
-        if (curBunnyCol > 0 && matrix[curBunnyRow, curBunnyCol - 1] != 'B')
-        {
-            if (matrix[curBunnyRow,curBunnyCol-1] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow, curBunnyCol - 1] = 'B';
-            allBunnies.Add($"{curBunnyRow}-{curBunnyCol - 1}");
-        }
-        //RIGHT
-        if (curBunnyCol < matrix.GetLength(1) - 1 && matrix[curBunnyRow, curBunnyCol + 1] != 'B')
-        {
-            if (matrix[curBunnyRow, curBunnyCol + 1] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow, curBunnyCol + 1] = 'B';
-            allBunnies.Add($"{curBunnyRow}-{curBunnyCol + 1}");
-        }
-        //UP
-        if (curBunnyRow > 0 && matrix[curBunnyRow - 1, curBunnyCol] != 'B')
-        {
-            if (matrix[curBunnyRow-1, curBunnyCol] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow - 1, curBunnyCol] = 'B';
-            allBunnies.Add($"{curBunnyRow - 1}-{curBunnyCol}");
-        }
-        //DOWN
-        if (curBunnyRow < matrix.GetLength(0) - 1 && matrix[curBunnyRow + 1, curBunnyCol] != 'B')
-        {
-            if (matrix[curBunnyRow + 1, curBunnyCol] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow + 1, curBunnyCol] = 'B';
-            allBunnies.Add($"{curBunnyRow + 1}-{curBunnyCol}");
-        }
-    }
-    currentBunnies.AddRange(allBunnies);
-    currentBunnies.Sort();
-    currentBunnies = currentBunnies.Distinct().ToList();
-}
-    
-    if (hasWon)
-    {
-    for (int j = 0; j < currentBunnies.Count; j++)
-    {
-        var curBunnyRow = int.Parse(currentBunnies[j].Split("-", StringSplitOptions.RemoveEmptyEntries)[0]);
-        var curBunnyCol = int.Parse(currentBunnies[j].Split("-", StringSplitOptions.RemoveEmptyEntries)[1]);
-        //LEFT
-        if (curBunnyCol > 0 && matrix[curBunnyRow, curBunnyCol - 1] != 'B')
-        {
-            if (matrix[curBunnyRow, curBunnyCol - 1] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow, curBunnyCol - 1] = 'B';
-            allBunnies.Add($"{curBunnyRow}-{curBunnyCol - 1}");
-        }
-        //RIGHT
-        if (curBunnyCol < matrix.GetLength(1) - 1 && matrix[curBunnyRow, curBunnyCol + 1] != 'B')
-        {
-            if (matrix[curBunnyRow, curBunnyCol + 1] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow, curBunnyCol + 1] = 'B';
-            allBunnies.Add($"{curBunnyRow}-{curBunnyCol + 1}");
-        }
-        //UP
-        if (curBunnyRow > 0 && matrix[curBunnyRow - 1, curBunnyCol] != 'B')
-        {
-            if (matrix[curBunnyRow - 1, curBunnyCol] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow - 1, curBunnyCol] = 'B';
-            allBunnies.Add($"{curBunnyRow - 1}-{curBunnyCol}");
-        }
-        //DOWN
-        if (curBunnyRow < matrix.GetLength(0) - 1 && matrix[curBunnyRow + 1, curBunnyCol] != 'B')
-        {
-            if (matrix[curBunnyRow + 1, curBunnyCol] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow + 1, curBunnyCol] = 'B';
-            allBunnies.Add($"{curBunnyRow + 1}-{curBunnyCol}");
-        }
-    }
-    PrintMatrix(matrix);
-    Console.WriteLine($"won: {startingRow} {startingColumn}");
-        return;
-    }
-    if (hasDied)
-    {
-    for (int j = 0; j < currentBunnies.Count; j++)
-    {
-        var curBunnyRow = int.Parse(currentBunnies[j].Split("-", StringSplitOptions.RemoveEmptyEntries)[0]);
-        var curBunnyCol = int.Parse(currentBunnies[j].Split("-", StringSplitOptions.RemoveEmptyEntries)[1]);
-        //LEFT
-        if (curBunnyCol > 0 && matrix[curBunnyRow, curBunnyCol - 1] != 'B')
-        {
-            if (matrix[curBunnyRow, curBunnyCol - 1] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow, curBunnyCol - 1] = 'B';
-            allBunnies.Add($"{curBunnyRow}-{curBunnyCol - 1}");
-        }
-        //RIGHT
-        if (curBunnyCol < matrix.GetLength(1) - 1 && matrix[curBunnyRow, curBunnyCol + 1] != 'B')
-        {
-            if (matrix[curBunnyRow, curBunnyCol + 1] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow, curBunnyCol + 1] = 'B';
-            allBunnies.Add($"{curBunnyRow}-{curBunnyCol + 1}");
-        }
-        //UP
-        if (curBunnyRow > 0 && matrix[curBunnyRow - 1, curBunnyCol] != 'B')
-        {
-            if (matrix[curBunnyRow - 1, curBunnyCol] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow - 1, curBunnyCol] = 'B';
-            allBunnies.Add($"{curBunnyRow - 1}-{curBunnyCol}");
-        }
-        //DOWN
-        if (curBunnyRow < matrix.GetLength(0) - 1 && matrix[curBunnyRow + 1, curBunnyCol] != 'B')
-        {
-            if (matrix[curBunnyRow + 1, curBunnyCol] == 'P')
-            {
-                hasDied = true;
-            }
-            matrix[curBunnyRow + 1, curBunnyCol] = 'B';
-            allBunnies.Add($"{curBunnyRow + 1}-{curBunnyCol}");
-        }
-    }
-    PrintMatrix(matrix);
-        Console.WriteLine($"dead: {startingRow} {startingColumn}");
-        return;
-    }
+var directions = Console.ReadLine();
 
+foreach (var item in directions)
+{
+    var oldRow = playerRow;
+    var oldCol = playerCol;
+    matrix[playerRow, playerCol] = '.';
+    //left
+    if (item == 'L')
+    {
+        playerCol--;
+    }
+    //right
+    else if (item == 'R')
+    {
+        playerCol++;
+    }
+    //up
+    else if (item == 'U')
+    {
+        playerRow--;
+    }
+    //down
+    else if (item == 'D')
+    {
+        playerRow++;
+    }
+    BunnyMove(matrix, currentBunnies, allBunnies);
+
+    allBunnies = allBunnies.Distinct().ToList();
+    currentBunnies.AddRange(allBunnies);
+    currentBunnies = currentBunnies.Distinct().ToList();
+    currentBunnies.Sort();
+    if(playerCol<0||playerCol>matrix.GetLength(1)-1||playerRow<0||playerRow> matrix.GetLength(0) - 1)
+    {
+        PrintMatrix(matrix);
+        Console.WriteLine($"won: {oldRow} {oldCol}");
+        break;
+    }
+    if (matrix[playerRow,playerCol] == 'B')
+    {
+        PrintMatrix(matrix);
+        Console.WriteLine($"dead: {playerRow} {playerCol}");
+        break;
+    }
+}
 static void PrintMatrix(char[,] matrix)
 {
     for (int row = 0; row < matrix.GetLength(0); row++)
@@ -286,4 +83,40 @@ static void PrintMatrix(char[,] matrix)
         }
         Console.WriteLine();
     }
+}
+static void BunnyMove(char[,] matrix, List<string> currentBunnies, List<string> allBunnies)
+{
+    foreach (var coord in currentBunnies)
+    {
+        
+        var bRow = int.Parse(coord.Split(",").ToArray()[0]);
+        var bCol = int.Parse(coord.Split(",").ToArray()[1]);
+
+        //left
+        if (bCol > 0)
+        {
+            matrix[bRow, bCol - 1] = 'B';
+            allBunnies.Add($"{bRow},{bCol - 1}");
+        }
+        //right
+        if (bCol < matrix.GetLength(1) - 1)
+        {
+            matrix[bRow, bCol + 1] = 'B';
+            allBunnies.Add($"{bRow},{bCol+1}");
+        }
+        //up
+        if (bRow > 0)
+        {
+            matrix[bRow - 1, bCol] = 'B';
+            allBunnies.Add($"{bRow - 1},{bCol}");
+        }
+        //down
+        if (bRow < matrix.GetLength(0) - 1)
+        {
+            matrix[bRow + 1, bCol] = 'B';
+            allBunnies.Add($"{bRow + 1},{bCol}");
+        }
+    }
+
+
 }
