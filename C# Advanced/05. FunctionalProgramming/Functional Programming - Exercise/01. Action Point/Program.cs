78db665fe405c74238f49ed9ea7a4529b20a8b12
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-06-06 16:45:52 +0300

    Functional Programming - Exercise
    7/11

---
+++ C# Advanced/05. FunctionalProgramming/Functional Programming - Exercise/01. Action Point/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01._Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> PrintOnNewRow = x => Console.WriteLine(x);
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(PrintOnNewRow);
+        }
+    }
+}
---
+++ C# Advanced/05. FunctionalProgramming/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> PutSir = x =>
+            {
+                Console.WriteLine("Sir " + x);
+            };
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(PutSir);
+        }
+    }
+}
