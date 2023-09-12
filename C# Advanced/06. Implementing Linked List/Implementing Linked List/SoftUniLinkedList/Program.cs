bd6fd04e56d3f950f8012526f7186930cfa215e6
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-09-12 19:07:16 +0300

    Implementing Linked List

---
+++ C# Advanced/06. Implementing Linked List/Implementing Linked List/SoftUniLinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SoftUniLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList linkedList = new CustomLinkedList();
+
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+            linkedList.AddFirst(0);
+            linkedList.AddFirst(-1);
+            linkedList.AddFirst(-2);
+            linkedList.AddFirst(-3);
+
+            linkedList.RemoveFirst();
+            linkedList.RemoveFirst();
+
+            linkedList.RemoveLast();
+
+            linkedList.ForEach(x => Console.WriteLine(x.Value));
+
+
+
+
+            Node currentNode = linkedList.Head;
+            while(currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next; 
+            }
+
+
+        }
+    }
+}
---
+++ C# Advanced/06. Implementing Linked List/Implementing Linked List/SoftUniLinkedList/SoftUniLinkedList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniLinkedList
+{
+    public class CustomLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public void ForEach(Action<Node> action)
+        {
+            Node node = Head;
+
+            while (node != null)
+            {
+                action(node);
+                node = node.Next;
+            }
+        }
+        public void AddLast(int value)
+        {
+            Node node = new Node(value);
+            if(Tail == null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+
+            Tail.Next = node;
+            node.Previous = Tail;
+            Tail = node;
+        }
+
+        public void AddFirst(int value)
+        {
+            Node node = new Node(value);
+            if(Head == null)
+            {
+                Head = node; 
+                Tail = node;
+                return;
+            }
+            Head.Previous = node;
+            node.Next = Head;
+            Head = node;
+        }
+
+        public void RemoveFirst()
+        {
+            if (Head.Next != null)
+            {
+                Node newHead = Head.Next;
+                newHead.Previous = null;
+                Head.Next = null;
+                Head = newHead;
+            }
+            else
+            {
+                Head = null;
+                Tail = null;
+            }
+
+        }
+
+        public void RemoveLast()
+        {
+            if(Tail.Previous == null)
+            {
+                Head = null;
+                Tail = null;
+            }
+            Node newTail = Tail.Previous;
+            newTail.Next = null;
+            Tail.Previous = null;
+            Tail = newTail;
+        }
+
+
+    }
+}
