bb6fa230abe7c1a3e7750162a611e97b76de246e
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-09-14 23:54:52 +0300

    Generics Lab
    2/3

---
+++ C# Advanced/08. Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> List { get; set; } = new List<T>();
+        public int Count { get; private set; } = 0;
+        public void Add(T item)
+        {
+            this.List.Add(item);
+            Count++;
+        }
+        public T Remove()
+        {
+            T lastElement = List.Last();
+            List.Remove(lastElement);
+            Count--;
+            return lastElement;
+        }
+    }
+}
---
+++ C# Advanced/08. Generics - Lab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+       
+        public static T[] Create<T>(int length,T item)
+        {
+            T[] ar = new T[length];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = item;
+            }
+            
+            return ar;
+            
+        }
+    }
+}
