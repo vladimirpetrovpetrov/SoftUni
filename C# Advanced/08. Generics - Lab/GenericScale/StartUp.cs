a2b36d733c77f8eb82d6d339a61d4da58cb2e444
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2023-09-15 22:25:39 +0300

    Generics - Lab
    Exercise 3

---
+++ C# Advanced/08. Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    internal class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            EqualityScale<string> scale = new EqualityScale<string>("tuk","tam");
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
