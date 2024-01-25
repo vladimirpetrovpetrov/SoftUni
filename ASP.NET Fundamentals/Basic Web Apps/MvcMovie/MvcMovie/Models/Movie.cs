c3649bcad92fb349bfb194b29b388069210e723b
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-25 11:34:10 +0200

    Added new field Rating to the movies

--- ASP.NET Fundamentals/Basic Web Apps/MvcMovie/MvcMovie/Models/Movie.cs
+++ ASP.NET Fundamentals/Basic Web Apps/MvcMovie/MvcMovie/Models/Movie.cs
@@ -15,4 +15,5 @@ public class Movie
     public string? Genre { get; set; }
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
 }
--- ASP.NET Fundamentals/Basic Web Apps/MvcMovie/MvcMovie/Models/SeedDat.cs
+++ ASP.NET Fundamentals/Basic Web Apps/MvcMovie/MvcMovie/Models/SeedDat.cs
@@ -25,6 +25,7 @@ public static class SeedData
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
+                    Rating = "R",
                     Price = 7.99M
                 },
                 new Movie
@@ -32,6 +33,7 @@ public static class SeedData
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "R",
                     Price = 8.99M
                 },
                 new Movie
@@ -39,6 +41,7 @@ public static class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "R",
                     Price = 9.99M
                 },
                 new Movie
@@ -46,6 +49,7 @@ public static class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "R",
                     Price = 3.99M
                 }
             );
