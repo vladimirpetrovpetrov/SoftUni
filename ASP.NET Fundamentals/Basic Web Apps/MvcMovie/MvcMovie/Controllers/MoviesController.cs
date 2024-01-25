762c245694ddcd5fbe354ecf2e1796b390bce659
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-25 11:41:17 +0200

    fixed create and edit

--- ASP.NET Fundamentals/Basic Web Apps/MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ ASP.NET Fundamentals/Basic Web Apps/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -89,7 +89,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -122,7 +122,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
