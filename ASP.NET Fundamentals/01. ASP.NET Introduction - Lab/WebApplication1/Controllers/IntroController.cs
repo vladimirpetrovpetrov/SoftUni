7be39a7ef293a2ecc4301d220feeb74b5a932d53
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-27 17:47:26 +0200

    Added one new action method
    Combining c# with html

--- ASP.NET Fundamentals/01. ASP.NET Introduction - Lab/WebApplication1/Controllers/IntroController.cs
+++ ASP.NET Fundamentals/01. ASP.NET Introduction - Lab/WebApplication1/Controllers/IntroController.cs
@@ -10,11 +10,21 @@ namespace AspNetCoreIntro2024.Controllers
             return View();
         }
 
-        public IActionResult GetNumber(int number) {
+        public IActionResult GetNumber(int number)
+        {
             {
                 ViewBag.Number = "GetNumber";
 
-                return View("Number",number);
-            } }
+                return View("Number", number);
+            }
+        }
+
+        public IActionResult Article()
+        {
+            ViewBag.Title = "The Little Prince";
+            ViewBag.Content = "The Little Prince, fable and modern classic by French aviator and writer Antoine de Saint-Exupéry that was published with his own illustrations in French as Le Petit Prince in 1943. The simple tale tells the story of a child, the little prince, who travels the universe gaining wisdom. The novella has been translated into hundreds of languages and has sold some 200 million copies worldwide, making it one of the best-selling books in publishing history.";
+            ViewBag.Author = "Saint-Exupéry";
+            return View();
+        }
     }
 }
