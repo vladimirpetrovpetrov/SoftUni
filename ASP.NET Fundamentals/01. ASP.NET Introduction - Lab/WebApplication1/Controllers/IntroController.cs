aaeda48e89bfcc208e6cfb8448537ebe8c93251c
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-27 17:38:01 +0200

    Example MVC project - added new controller and new action methods

---
+++ ASP.NET Fundamentals/01. ASP.NET Introduction - Lab/WebApplication1/Controllers/IntroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIntro2024.Controllers
+{
+    public class IntroController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Intro";
+            return View();
+        }
+
+        public IActionResult GetNumber(int number) {
+            {
+                ViewBag.Number = "GetNumber";
+
+                return View("Number",number);
+            } }
+    }
+}
