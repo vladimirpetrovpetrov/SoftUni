dab26da42dae2b49c0127165b4bc7849f694f6d9
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-28 20:30:53 +0200

    Added new controller with action methods

---
+++ ASP.NET Fundamentals/02. ASP.NET Introduction - Exercise/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            return View(_products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = _products.FirstOrDefault(x => x.Id == id);
+            if(product == null)
+            {
+                return BadRequest();
+            }
+
+            return View(product);
+        }
+
+        private IEnumerable<ProductViewModel> _products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id= 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+
+            new ProductViewModel()
+            {
+                Id= 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+
+            new ProductViewModel()
+            {
+                Id= 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+    }
+}
---
+++ ASP.NET Fundamentals/02. ASP.NET Introduction - Exercise/MVCIntroDemo/MVCIntroDemo/Models/Product/ProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCIntroDemo.Models.Product;
+
+public class ProductViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
