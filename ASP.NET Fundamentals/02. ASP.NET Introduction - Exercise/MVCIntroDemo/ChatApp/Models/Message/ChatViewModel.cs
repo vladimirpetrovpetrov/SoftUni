4926b36b74f7fbabcb8f2bb1e5ee078d677654e8
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-30 14:46:39 +0200

    ChatApp
    Added action methods , models and a view.

---
+++ ASP.NET Fundamentals/02. ASP.NET Introduction - Exercise/MVCIntroDemo/ChatApp/Models/Message/ChatViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatApp.Models.Message;
+
+public class ChatViewModel
+{
+    public MessageViewModel CurrentMessage { get; set; } = null!;
+    public List<MessageViewModel> Messages { get; set; } = null!;
+}
