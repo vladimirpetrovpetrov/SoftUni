9798dbc32ea612f5fe2261b8d31baeb2a302636e
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-30 20:10:23 +0200

    ShoppingListApp
    Created new Web project and its database with EF Core.

---
+++ ASP.NET Fundamentals/03. ASP.NET and Databases/ShoppingListApp/ShoppingListApp/Data/ShoppingListDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingListApp.Data.Models;
+
+namespace ShoppingListApp.Data
+{
+    public class ShoppingListDbContext : DbContext
+    {
+        public ShoppingListDbContext(DbContextOptions<ShoppingListDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductNote> ProductNotes { get; set; }
+
+
+
+    }
+}
