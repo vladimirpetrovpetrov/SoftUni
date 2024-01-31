2dbb3747b1a3b2213a5996b3655edfc24958902c
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-01-31 13:06:17 +0200

    Workshop - Forum App
    Initial commit & Added the entity model

---
+++ ASP.NET Fundamentals/04. Workshop - Forum App/ForumApp/ForumApp/Data/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ForumApp.Data
+{
+    public class DataConstants
+    {
+        public class Post
+        {
+            public const int TitleMaxLength = 50;
+            public const int ContentMaxLength = 1500;
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/04. Workshop - Forum App/ForumApp/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Data.Models;
+
+public class Post
+{
+    [Comment("Post identifier")]
+    [Key]
+    public int Id { get; init; }
+    [Comment("Post title")]
+    [MaxLength(DataConstants.Post.TitleMaxLength)]
+    [Required]
+    public string Title { get; set; } = null!;
+    [Comment("Post content")]
+    [MaxLength(DataConstants.Post.ContentMaxLength)]
+    [Required]
+    public string Content { get; set; } = null!;
+}
