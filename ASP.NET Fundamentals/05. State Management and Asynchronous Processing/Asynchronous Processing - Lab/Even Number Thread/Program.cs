2c828e4e92921794f5b3aee2104507bf49dbfe6e
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-02-01 20:31:15 +0200

    State Management and Asynchronous Processing
    Asynchronous Processing - Lab

---
+++ ASP.NET Fundamentals/05. State Management and Asynchronous Processing/Asynchronous Processing - Lab/Even Number Thread/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Even_Number_Thread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            Thread evens = new Thread(() => PrintEvenNumbers(n, m));
+            evens.Start();
+            evens.Join();
+            Console.WriteLine("Thread finished work");
+        }
+
+        private static void PrintEvenNumbers(int n, int m)
+        {
+            for (int i = n; i <= m; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/05. State Management and Asynchronous Processing/Asynchronous Processing - Lab/Sum Evens in Range/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Sum_Evens_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "show")
+                {
+                    var result = SymAsync();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        private static long SymAsync()
+        {
+            return Task.Run(() =>
+            {
+                long sum = 0;
+                for (int i = 1; i < 10000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                return sum;
+            }).Result;
+        }
+    }
+}
