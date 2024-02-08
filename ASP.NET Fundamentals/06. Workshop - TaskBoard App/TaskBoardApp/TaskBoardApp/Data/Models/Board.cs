34c4f667ce4e847ceb0d5a5bb406f156bf34c457
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-02-08 22:29:15 +0200

    Entity fixes
    Added comments , made some of the properties nullable as it is in the task assignment.

--- ASP.NET Fundamentals/06. Workshop - TaskBoard App/TaskBoardApp/TaskBoardApp/Data/Models/Board.cs
+++ ASP.NET Fundamentals/06. Workshop - TaskBoard App/TaskBoardApp/TaskBoardApp/Data/Models/Board.cs
@@ -1,14 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using static TaskBoardApp.Data.DataConstants;
 
 namespace TaskBoardApp.Data.Models;
 
+[Comment("App board")]
 public class Board
 {
     [Key]
+    [Comment("Board identifier")]
     public int Id { get; set; }
     [Required]
     [StringLength(BoardNameMaxLength)]
-    public string Name { get; set; } = null!;
+    [Comment("Board name")]
+    public string Name { get; set; } = string.Empty;
     public ICollection<Task> Tasks { get; set; } = new List<Task>();
 }
--- ASP.NET Fundamentals/06. Workshop - TaskBoard App/TaskBoardApp/TaskBoardApp/Data/Models/Task.cs
+++ ASP.NET Fundamentals/06. Workshop - TaskBoard App/TaskBoardApp/TaskBoardApp/Data/Models/Task.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using static TaskBoardApp.Data.DataConstants;
 
 namespace TaskBoardApp.Data.Models;
 
+[Comment("Board Tasks")]
 public class Task
 {
     [Key]
+    [Comment("Task identifier")]
     public int Id { get; set; }
     [Required]
     [StringLength(TaskTitleMaxLength)]
-    public string Title { get; set; } = null!;
+    [Comment("Task title")]
+    public string Title { get; set; } = string.Empty;
     [Required]
     [StringLength(TaskDescMaxLength)]
-    public string Description { get; set; } = null!;
-    public DateTime CreatedOn { get; set; }
-    public int BoardId { get; set; }
+    [Comment("Task description")]
+    public string Description { get; set; } = string.Empty;
+    [Comment("Date of creation")]
+    public DateTime? CreatedOn { get; set; }
+    [Comment("Board identifier")]
+    public int? BoardId { get; set; }
+    [ForeignKey(nameof(BoardId))]
     public Board? Board { get; set; }
     [Required]
-    public string OwnerId { get; set; } = null!;
+    [Comment("Application user identifier")]
+    public string OwnerId { get; set; } = string.Empty;
+    [ForeignKey(nameof(OwnerId))]
     public IdentityUser Owner { get; set; } = null!;
 }
