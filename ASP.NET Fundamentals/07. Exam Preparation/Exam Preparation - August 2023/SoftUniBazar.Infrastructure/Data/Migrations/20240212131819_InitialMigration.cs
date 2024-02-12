4ddb8c306de300626367aa94fce6ea05c041bfd5
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-02-12 15:38:25 +0200

    SoftUniBazar Exam Prep
    Cleaned Login & Register and set the registration requirements

---
+++ ASP.NET Fundamentals/07. Exam Preparation/Exam Preparation - August 2023/SoftUniBazar.Infrastructure/Data/Migrations/20240212131819_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftUniBazar.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Ad identifier")
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ads", x => x.Id);
+                },
+                comment: "Model for advertisement");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ads");
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/07. Exam Preparation/Exam Preparation - August 2023/SoftUniBazar.Infrastructure/Data/Models/Ad.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftUniBazar.Infrastructure.Data.Models;
+
+[Comment("Model for advertisement")]
+public class Ad
+{
+    [Key]
+    [Comment("Ad identifier")]
+    public int Id { get; set; }
+}
