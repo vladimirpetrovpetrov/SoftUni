294ac6d91b43b58db1883343a5dc2741b816c6f7
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-02-21 21:06:23 +0200

    JS Syntax Fundamentals
    Task 02

---
+++ JS Front-End/01. JS Syntax Fundamentals - Lab/02. Student Information.js
@@ -0,0 +1,9 @@
+function solve(name,age,grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+let name = "John Doe"
+let age = 22;
+let grade = 4.48;
+
+solve(name,age,grade);
