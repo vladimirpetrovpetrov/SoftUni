fc5755be9f8d478e39bd9fb0547f8c8f11c0fcad
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-02-22 15:04:53 +0200

    Task 07

---
+++ JS Front-End/01. JS Syntax Fundamentals - Lab/07. Theatre Promotions.js
@@ -0,0 +1,45 @@
+function solve(day, age) {
+    switch (day) {
+        case 'Weekday':
+            if (age >= 0 && age <= 18){
+                console.log('12$')
+            }else if (age >= 19  && age <= 64){
+                console.log('18$')
+            }else if (age >= 65 && age <= 122){
+                console.log('12$')
+            }else{
+                console.log('Error!');
+            }
+                break;
+        case 'Weekend':
+            if (age >= 0 && age <= 18){
+                console.log('15$')
+            }else if (age >= 19  && age <= 64){
+                console.log('20$')
+            }else if (age >= 65 && age <= 122){
+                console.log('15$')
+            }else{
+                console.log('Error!');
+            }
+            break;
+        case 'Holiday':
+            if (age >= 0 && age <= 18){
+                console.log('5$')
+            }else if (age >= 19  && age <= 64){
+                console.log('12$')
+            }else if (age >= 65 && age <= 122){
+                console.log('10$')
+            }else{
+                console.log('Error!');
+            }
+            break;
+
+        default:
+            console.log("Error!")
+            break;
+    }
+}
+
+solve('Weekday',42)
+solve('Holiday', -12)
+solve('Holiday',15)
