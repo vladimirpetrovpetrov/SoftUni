fc7948b6d578b0a328360c37d24a98e27ea5fd44
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-10 22:53:40 +0200

    House Service and HouseIndexModel added

---
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Services/House/HouseService.cs
@@ -0,0 +1,31 @@
+﻿using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services.House;
+
+public class HouseService : IHouseService
+{
+    private readonly IRepository repository;
+
+    public HouseService(IRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+    {
+        return await repository
+            .AllReadOnly<Infrastructure.Data.Models.House>()
+            .OrderByDescending(x => x.Id)
+            .Take(3)
+            .Select(h => new HouseIndexServiceModel
+            {
+                Id = h.Id,
+                ImageUrl = h.ImageUrl,
+                Title = h.Title,
+            })
+            .ToListAsync();
+    }
+}
