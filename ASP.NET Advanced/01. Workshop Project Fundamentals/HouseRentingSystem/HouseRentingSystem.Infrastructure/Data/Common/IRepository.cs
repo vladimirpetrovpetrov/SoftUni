36e71aa381a921c688f8fe8971ff1b69f34fe102
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-10 22:51:15 +0200

    Repository added

---
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace HouseRentingSystem.Infrastructure.Data.Common;
+
+public interface IRepository
+{
+    IQueryable<T> All<T>() where T : class;
+    IQueryable<T> AllReadOnly<T>() where T : class;
+}
---
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data.Common;
+
+public class Repository : IRepository
+{
+    private readonly DbContext context;
+
+    public Repository(HouseRentingDbContext context)
+    {
+        this.context = context;
+    }
+    private DbSet<T> DbSet<T>() where T : class
+    {
+        return context.Set<T>();
+    }
+
+    public IQueryable<T> All<T>() where T : class
+    {
+        return DbSet<T>();
+    }
+    public IQueryable<T> AllReadOnly<T>() where T : class
+    {
+        return DbSet<T>()
+            .AsNoTracking();
+    }
+}
