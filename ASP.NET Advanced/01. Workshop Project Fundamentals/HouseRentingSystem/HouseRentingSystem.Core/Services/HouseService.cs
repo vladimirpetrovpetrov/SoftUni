ffbfb52f90553d92ff6aa10d1b5c45e1a427e2ca
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-12 05:15:50 +0200

    Namespaces adjusted

--- ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Services/HouseService.cs
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Services/HouseService.cs
@@ -1,9 +1,10 @@
-﻿using HouseRentingSystem.Core.Contracts.House;
+﻿using HouseRentingSystem.Core.Contracts;
 using HouseRentingSystem.Core.Models.Home;
 using HouseRentingSystem.Infrastructure.Data.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace HouseRentingSystem.Core.Services.House;
+namespace HouseRentingSystem.Core.Services;
 
 public class HouseService : IHouseService
 {
@@ -17,7 +18,7 @@ public class HouseService : IHouseService
     public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
     {
         return await repository
-            .AllReadOnly<Infrastructure.Data.Models.House>()
+            .AllReadOnly<House>()
             .OrderByDescending(x => x.Id)
             .Take(3)
             .Select(h => new HouseIndexServiceModel
