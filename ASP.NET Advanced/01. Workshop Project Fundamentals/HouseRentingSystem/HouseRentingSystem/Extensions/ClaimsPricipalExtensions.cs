e066422731f402039e46c6004af3f3c8cefdeda2
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-12 05:36:07 +0200

    Claims Principal Extented

---
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Extensions/ClaimsPricipalExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace HouseRentingSystem.Extensions;
+
+public static class ClaimsPricipalExtensions
+{
+    public static string Id(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
