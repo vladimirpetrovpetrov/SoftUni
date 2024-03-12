73d736e0cf3c5553c1c3db694061deb6f35c28ae
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-12 05:23:19 +0200

    Base Controller added

---
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers;
+[Authorize]
+public class BaseController : Controller
+{
+
+}
--- ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
 ﻿using HouseRentingSystem.Core.Contracts;
-using HouseRentingSystem.Core.Models.Home;
 using HouseRentingSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HouseRentingSystem.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IHouseService houseService;
@@ -20,7 +20,7 @@ namespace HouseRentingSystem.Controllers
             _logger = logger;
             this.houseService = houseService;
         }
-
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var model = await houseService.LastThreeHouses();
@@ -28,6 +28,7 @@ namespace HouseRentingSystem.Controllers
             return View(model);
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
