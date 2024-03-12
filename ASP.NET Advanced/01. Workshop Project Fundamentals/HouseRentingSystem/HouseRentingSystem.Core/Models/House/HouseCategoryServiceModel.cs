207c746fdd30502ae5c94fbab6737aa2bf9e992c
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-12 18:03:40 +0200

    House services added and implemented and house models added

---
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseCategoryServiceModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HouseRentingSystem.Core.Models.House;
+
+public class HouseCategoryServiceModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
--- ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseFormModel.cs
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseFormModel.cs
@@ -1,6 +1,41 @@
-﻿namespace HouseRentingSystem.Core.Models.House;
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants.House;
+
+
+namespace HouseRentingSystem.Core.Models.House;
 
 public class HouseFormModel
 {
+    [Required(ErrorMessage = RequiredMessage)]
+    [StringLength(MaxTitleLength,
+        MinimumLength = MinTitleLength,
+        ErrorMessage = LengthMessage)]
+    public string Title { get; set; } = null!;
 
+    [Required(ErrorMessage = RequiredMessage)]
+    [StringLength(MaxAddressLength,
+        MinimumLength = MinAddressLength,
+        ErrorMessage = LengthMessage)]
+    public string Address { get; set; } = null!;
+
+    [Required(ErrorMessage = RequiredMessage)]
+    [StringLength(MaxDescLength,
+        MinimumLength = MinDescLength,
+        ErrorMessage = LengthMessage)]
+    public string Description { get; set; } = null!;
+
+    [Required(ErrorMessage = RequiredMessage)]
+    [Display(Name = "Image URL")]
+    public string ImageUrl { get; set; } = null!;
+
+    [Required(ErrorMessage = RequiredMessage)]
+    [Range(typeof(decimal),MinPricePerMonth, MaxPricePerMonth)]
+    [Display(Name = "Price Per Month")]
+    public decimal PricePerMonth { get; set; }
+
+    [Display(Name = "Category")]
+    public int CategoryId { get; set; }
+
+    public IEnumerable<HouseCategoryServiceModel> Categories { get; set; } = new List<HouseCategoryServiceModel>();
 }
