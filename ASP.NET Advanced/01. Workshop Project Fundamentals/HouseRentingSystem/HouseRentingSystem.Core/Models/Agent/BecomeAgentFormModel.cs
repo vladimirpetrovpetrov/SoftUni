7a0896e4e36cad741a4773d4ac442bd1d070ce5e
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-12 06:02:28 +0200

    BecomeAgentModel created

--- ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,5 +1,17 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent;
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Constants.DataConstants.Agent;
+
+namespace HouseRentingSystem.Core.Models.Agent;
 
 public class BecomeAgentFormModel
 {
+    [Required(ErrorMessage = RequiredMessage)]
+    [StringLength(
+        MaxPhoneNumberLength,
+        MinimumLength = MinPhoneNumberLength,
+        ErrorMessage = LengthMessage)]
+    [Display(Name = "Phone number")]
+    [Phone]
+    public string PhoneNumber { get; set; } = null!;
 }
