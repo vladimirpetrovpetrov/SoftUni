d391683095be923305a293131cbf2869205acb1c
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2024-03-14 23:40:42 +0200

    Custom error pages added when in production.

--- ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using HouseRentingSystem.Core.Contracts;
-using HouseRentingSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace HouseRentingSystem.Controllers
 {
@@ -30,9 +28,19 @@ namespace HouseRentingSystem.Controllers
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
         }
     }
 }
--- ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Program.cs
+++ ASP.NET Advanced/01. Workshop Project Fundamentals/HouseRentingSystem/HouseRentingSystem/Program.cs
@@ -17,10 +17,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
     app.UseHsts();
 }
 
